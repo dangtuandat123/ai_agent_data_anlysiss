38133a5ee0b963a5f6d08ed24450ddd3ee0854b9
dangtuandat123 <datxtofficial@gmail.com>
2025-08-09 10:11:20 +0700

    add them giao dien load anh tren message ai

--- src/components/MessageAI.vue
+++ src/components/MessageAI.vue
@@ -19,17 +19,7 @@ import hljs from 'highlight.js'; // Import Highlight.js
 import 'highlight.js/styles/monokai-sublime.css'; // Import style theme của Highlight.js
 
 export default {
-    data() {
-        return {
-            // Thay đổi code mẫu tại đây
-            code: `
-            function sayHello() {
-              console.log('Hello World!');
-            }
-            sayHello();
-            `,
-        };
-    },
+
     props: ['message'],
     components: { ButtonCopy },
     computed: {
@@ -37,36 +27,55 @@ export default {
             // Kiểm tra nếu message là mã code
             if (this.message.iscode) {
                 // Render cả code và message. Code được render dưới dạng <pre><code>...</code></pre> và message dưới dạng HTML
-                const messageHtml = DOMPurify.sanitize(this.message.message || '');
+                const messageHtml = marked(this.message.message || '');
                 const codeHtml = this.highlightCode(this.message.code || '');  // Hiển thị code với Highlight.js
+                const rawhtmlMessage = DOMPurify.sanitize(messageHtml);
+                const img_base64 = this.message.img_base64 
+
+
                 return `
-                    <div class="message-content">
-                            <div style="display: flex; align-items: center; margin-bottom:5px">
-                                <img src="https://logos-world.net/wp-content/uploads/2021/10/Python-Emblem.png"  alt="" style="height: 18px;">
-                                <strong>Python</strong>
-                            </div>
-                           
+            <div class="message-content">
+                <div style="display: flex; align-items: center; margin-bottom:5px">
+                    <img src="https://logos-world.net/wp-content/uploads/2021/10/Python-Emblem.png"  alt="" style="height: 18px;">
+                    <strong>Python</strong>
+                </div>
+                <div class="code_div" style="border: 1px solid rgba(229, 231, 235, 0.2); border-radius: 10px; padding: 10px;background: #282c34; font-size:17px;margin-bottom:5px;overflow-x: hidden;overflow-y: auto;max-height: 290px;">
+                    <pre><code>${codeHtml}</code></pre>
+                </div>
+                 <div style="display: flex; align-items: center; margin-bottom:5px;width: 100%;height: auto;max-width: 100%; padding:10px 10% 10px 10%">
+                    <img src="data:image/png;base64,${img_base64}" alt="" style="width: 100%;height: auto;max-width: 100%; border-radius:10px">
+                </div>
+                <div class="message-text">${rawhtmlMessage}</div> <!-- Message được render dưới dạng HTML -->
+               
+                
 
-                        <div class="code_div" style="border: 1px solid rgba(229, 231, 235, 0.2); border-radius: 10px; padding: 10px;background: #282c34; font-size:17px;margin-bottom:5px"">
-                            <pre><code>${codeHtml}</code></pre> 
-                            </div>
-                        <div class="message-text">${messageHtml}</div> <!-- Message được render dưới dạng HTML -->
-                    </div>
-                `;
+            </div>
+            <style>
+                .code_div::-webkit-scrollbar-thumb {
+                    background-color: #888 !important;
+                    /* Màu sắc của phần tay cầm thanh cuộn */
+                    border-radius: 3px !important;
+                    /* Bo tròn tay cầm */
+                }
+            </style>
+        `;
             }
 
-            // Kiểm tra nếu message là file (hoặc nội dung Markdown)
-            else if (this.message.isfile) {
-                const rawHtml = marked(this.message.message || '');
-                // Làm sạch HTML từ Markdown và trả về
-                return DOMPurify.sanitize(rawHtml);
-            }
+            // // Kiểm tra nếu message là file (hoặc nội dung Markdown)
+            // else if (this.message.isfile) {
+            //     const rawHtml = marked(this.message.message || '');
+            //     // Làm sạch HTML từ Markdown và trả về
+            //     return DOMPurify.sanitize(rawHtml);
+            // }
 
             // Nếu không phải code hay file, chỉ đơn giản là trả về message bình thường
             else {
-                return DOMPurify.sanitize(this.message.message || '');
+                const rawHtml = marked(this.message.message || '');
+
+                return DOMPurify.sanitize(rawHtml);
             }
         }
+
     },
     methods: {
         // Hàm highlight mã nguồn với Highlight.js
@@ -128,6 +137,7 @@ export default {
 
 .message-content {
     word-break: break-word;
+    
 }
 
 /* .code_div {
